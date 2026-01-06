1035ff5369cb765de9464f19f8831bf84c0c68c8
Michael Fomichev <mialfomichev@edu.hse.ru>
2026-01-06 17:08:27 +0300

    added simple aggrigations, added logic for calculating simple queries in clickbench

---
+++ src/engine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ragnarokk on 06.01.2026.
+//
+
+#include "engine.h"
---
+++ src/engine.h
@@ -0,0 +1,16 @@
+//
+// Created by ragnarokk on 06.01.2026.
+//
+
+#ifndef COLUMNAR_ENGINE_ENGINE_H
+#define COLUMNAR_ENGINE_ENGINE_H
+
+
+
+class engine {
+
+};
+
+
+
+#endif //COLUMNAR_ENGINE_ENGINE_H
